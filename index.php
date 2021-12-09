647c8350e3a9af8982abb15bcb0830faf2683c2a
brunopereira03 <77056372+brunopereira03@users.noreply.github.com>
2021-12-09 10:52:01 +0000

    navbar

--- index.php
+++ index.php
@@ -1,43 +1,68 @@
-<?php
-require_once './database/connection.php';
-
-$statement = $pdo->prepare('SELECT * FROM produto');
-$statement->execute();
-$produtos = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <meta name="description" content="Trabalho Prático 221B">
+    <meta name="author" content="Bruno Pereira">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Trabalho Prático 221B</title>
+
 </head>
 <body>
-    
-<div class="container">
-    <h1 class="mt-5">Lista de Produtos</h1>
-    <a href="create.php" class="btn btn-outline-success mt-3">Criar produto</a>
 
-    <ul class="list-group">
-        <?php
-            foreach($produtos as $i => $prod) { ?>
-                <li class="list-group-item">
-                    <div class="container">
-                        <h4><?php echo $i.' - '.$prod['nome'] ?></h4>
-                        <span class="badge rounded-pill bg-secondary">
-                            <?php echo $prod['preco'] ?>
-                        </span>
-                    
-                        <?php if ($prod['descricao']) ?>
-                        <p class="mt-4"><?php echo $prod['descricao'] ?></p>
-                    </div>
-                </li>
-        <?php    } ?>
+<!-- Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid justify-content-between">
+    <!-- Left elements -->
+    <div class="d-flex">
+      <!-- Brand -->
+      <a class="navbar-brand me-2 mb-1 d-flex align-items-center" href="#">
+        <img
+          src="lupa.png"
+          height="20"
+          alt="221B Logo"
+          loading="lazy"
+          style="margin-top: 2px;"
+        />
+      </a>
+
+      <!-- Search form -->
+      <form class="input-group w-auto my-auto d-none d-sm-flex">
+        <input
+          autocomplete="off"
+          type="search"
+          class="form-control rounded"
+          placeholder="Search"
+          style="min-width: 125px;"
+        />
+      </form>
+    </div>
+    <!-- Center elements -->
+
+    <div class="navbar-nav text-white flex-row d-none d-md-flex">
+    <strong>The tings finder</strong>
+    </div>
+
+    
+
+    <!-- Right elements -->
+    <ul class="navbar-nav text- white flex-row">
+      <li class="nav-item me-3 me-lg-1">
+        <a class="nav-link d-sm-flex align-items-sm-center" href="#">
+          <strong class="d-none d-sm-block ms-1">Criar Conta</strong>
+        </a>
+      </li>
+        </ul>
+      </li>
     </ul>
-</div>
+    <!-- Right elements -->
+  </div>
+</nav>
+    
 </body>
 </html>
-
