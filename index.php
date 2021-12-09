0bdb8f7c3b1b5457f2f3172fe7db54ee786eee2c
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2021-12-09 22:11:30 +0000

    login form attempt

--- index.php
+++ index.php
@@ -1,37 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Trabalho Prático 221B">
-    <meta name="author" content="Bruno Pereira">
-
-    <link href="./css/style.css" rel="stylesheet">
-    
-    <title>Trabalho Prático 221B</title>
-
-</head>
-<body>
+<?php require_once './components/header.php' ?>
 
 <header id="navbar">
   <nav class="navbar-container container">
-    <a href="/" class="home-link">
-      <div class="navbar-logo"></div>
-      Website Name
+    <a href="/TP1_SIR" class="home-link">
+      <img class="navbar-logo" src="./assets/logo.png">
+      221B
     </a>
-    <button type="button" id="navbar-toggle" aria-controls="navbar-menu" aria-label="Toggle menu" aria-expanded="false">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
     <div id="navbar-menu" aria-labelledby="navbar-toggle">
       <ul class="navbar-links">
-        <li class="navbar-item"><a class="navbar-link" href="/about">About</a></li>
-        <li class="navbar-item"><a class="navbar-link" href="/blog">Blog</a></li>
-        <li class="navbar-item"><a class="navbar-link" href="/careers">Careers</a></li>
-        <li class="navbar-item"><a class="navbar-link" href="/contact">Contact</a></li>
+        <li class="navbar-item"><a class="navbar-link" href="/about">Apontamentos</a></li>
+      <!--  <li class="navbar-item"><a class="navbar-link" href="/blog">Blog</a></li>
+        <li class="navbar-item"><a class="navbar-link" href="/careers">Careers</a></li> -->
+        <li class="navbar-item"><a class="navbar-link" href="/contact">Sair</a></li>
       </ul>
     </div>
   </nav>
