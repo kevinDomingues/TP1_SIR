460617391b37b762987b47711d04124b9a484b41
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2021-12-19 03:50:23 +0000

    responsive navbar

---
+++ components/nav.php
@@ -0,0 +1,20 @@
+<header id="navbar">
+  <nav class="navbar-container container">
+    <a href="/TP1_SIR" class="home-link">
+      <img class="navbar-logo" src="./assets/logo.png">
+      221B
+    </a>
+    <button type="button" id="navbar-toggle" aria-controls="navbar-menu" aria-label="Toggle menu" aria-expanded="false">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <div id="navbar-menu" aria-labelledby="navbar-toggle">
+      <ul class="navbar-links" id="dropdown">
+        <li class="navbar-item"><a class="navbar-link" href="/TP1_SIR">Apontamentos</a></li>
+        <li class="navbar-item"><a class="navbar-link" href="/TP1_SIR">Perfil</a></li>
+        <li class="navbar-item"><a class="navbar-link" href="/TP1_SIR">Sair</a></li>
+      </ul>
+    </div>
+  </nav>
+</header>
