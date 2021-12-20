842a37b0691170199c72e6334d6a8fd137898f73
brunopereira03 <77056372+brunopereira03@users.noreply.github.com>
2021-12-20 01:52:30 +0000

    tentativadedashboard

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,33 @@
-<?php echo 'dashboard' ?>
+<?php require_once './components/header.php'?>
+<body class="dashboardbackground">
+    
+
+<header id="navbar">
+  <nav class="navbar-container container">
+    <a href="/TP1_SIR" class="home-link">
+      <img class="navbar-logo" src="./assets/logo.png">
+      221B
+    </a>
+    <div id="navbar-menu" aria-labelledby="navbar-toggle">
+      <ul class="navbar-links">
+        <li class="navbar-item"><a class="navbar-link" href="/about">Apontamentos</a></li>
+        <li class="navbar-item"><a class="navbar-link" href="/blog">Blog</a></li>
+        <li class="navbar-item"><a class="navbar-link" href="/careers">Careers</a></li>  
+        <li class="navbar-item"><a class="navbar-link" href="index.php">Sair</a></li>
+      </ul>
+    </div>
+  </nav>
+</header>
+
+<div class="dashboardcontainer">
+    <div></div>
+    <div class="middlediv"></div>
+    <div></div>
+</div>
+
+<div class="dashboardfeed">
+    <div></div>
+</div>
+
+</body>
+</html>
