f5299df9dc8b2ae5ab4f645589f6d1828efe9319
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2021-12-20 13:03:11 +0000

    Navbar fix dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,23 +1,9 @@
-<?php require_once './components/header.php'?>
-<body class="dashboardbackground">
-    
+<?php 
+require_once './components/header.php';
+require_once './components/nav.php';
 
-<header id="navbar">
-  <nav class="navbar-container container">
-    <a href="/TP1_SIR" class="home-link">
-      <img class="navbar-logo" src="./assets/logo.png">
-      221B
-    </a>
-    <div id="navbar-menu" aria-labelledby="navbar-toggle">
-      <ul class="navbar-links">
-        <li class="navbar-item"><a class="navbar-link" href="/about">Apontamentos</a></li>
-        <li class="navbar-item"><a class="navbar-link" href="/blog">Blog</a></li>
-        <li class="navbar-item"><a class="navbar-link" href="/careers">Careers</a></li>  
-        <li class="navbar-item"><a class="navbar-link" href="index.php">Sair</a></li>
-      </ul>
-    </div>
-  </nav>
-</header>
+?>
+<body class="dashboardbackground">
 
 <div class="dashboardcontainer">
     <div></div>
@@ -29,5 +15,8 @@
     <div></div>
 </div>
 
+<?php
+require_once './components/scripts.php';
+?>
 </body>
 </html>
