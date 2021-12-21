d510c00fc63215e70c5617fbbfa97d15af9f3ddf
brunopereira03 <77056372+brunopereira03@users.noreply.github.com>
2021-12-21 01:38:39 +0000

    nao acabei

--- dashboard.php
+++ dashboard.php
@@ -3,20 +3,128 @@ require_once './components/header.php';
 require_once './components/nav.php';
 
 ?>
-<body class="dashboardbackground">
 
-<div class="dashboardcontainer">
-    <div></div>
-    <div class="middlediv"></div>
-    <div></div>
+
+<div class="bodydashboard">
+
+    <div class="dashboardcontainer">
+        <div class="dashboardcard left-menu">
+            <div class="dashboardcard-left-menu-header">
+                <img src="https://randomuser.me/api/portraits/women/8.jpg" alt="rover" />
+                <h1>Sara Silva</h1>
+                <h6>Ultimo apontamento "--/--/----"</h5>
+            </div>
+            <div class="dashboardcard-body">
+                <span class="dashboardtag dashboardtag-teal">Technology</span>
+                <h4>
+                    Why is the Tesla Cybertruck designed the way it
+                    is?
+                </h4>
+                <p>
+                    An exploration into the truck's polarising design
+                </p>
+                <div class="dashboarduser">
+                    <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+                    <div class="dashboarduser-info">
+                    <h5>July Dec</h5>
+                    <small>2h ago</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="dashboardcard feed">
+            <div class="dashboardcard-header">
+               <!--  <img src="https://www.newsbtc.com/wp-content/uploads/2020/06/mesut-kaya-LcCdl__-kO0-unsplash-scaled.jpg" alt="ballons" /> -->
+               <h1>"imagem do apontamento"</h1>
+            </div>
+            <div class="dashboardcard-body">
+                <span class="dashboardtag dashboardtag-purple">Popular</span>
+                <h4>
+                    Titulo do apontamento
+                </h4>
+                <p>
+                    descrição do apontamento
+                </p>
+                <div class="dashboarduser">
+                    <small>Data hora que foi feito</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="dashboardcard right-menu">
+            
+            <div class="dashboardcard-header">
+                <img src="https://images6.alphacoders.com/312/thumb-1920-312773.jpg" alt="city" />
+            </div>
+            <div class="dashboardcard-body">
+                <span class="dashboardtag dashboardtag-pink">Design</span>
+                <h4>
+                    10 Rules of Dashboard Design
+                </h4>
+                <p>
+                    Dashboard Design Guidelines
+                </p>
+                <div class="dashboarduser">
+                    <img src="https://studyinbaltics.ee/wp-content/uploads/2020/03/3799Ffxy.jpg" alt="user" />
+                    <div class="dashboarduser-info">
+                    <h5>Carrie Brewer</h5>
+                    <small>1w ago</small>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
 </div>
+
+
+
+
+
+
+<!--<body class="dashboardbackground">
 
 <div class="dashboardfeed">
-    <div></div>
+    <div>
+        <h1>Feed</h1>
+    </div>
 </div>
+
+<div class="leftPanel">
+    <div class="dashboardLMenu">
+        <div>
+            <h1>Profile</h1>
+        </div>
+    </div>
+</div>
+
+<div class="rightPanel">
+    <div class="dashboardRMenu">
+        <div>
+            <h1>Menu</h1>
+        </div>
+    </div>
+</div>
+
+<div class="dashboardcontainers">
+    <div>
+        <div class="fotoPerfil">
+            <img class="fotoPerfilImg" src="./assets/logo.png">
+        </div>
+        
+        
+    </div>
+    <div class="middlediv"></div>
+    <div>
+        <h1>Settings</h1>
+    </div>
+</div>-->
 
 <?php
 require_once './components/scripts.php';
 ?>
+
 </body>
 </html>
